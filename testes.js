d3f07dc241a69ad15d87d4c6c232e8873294a271
wellington carlos <wellingtoncarlos70@gmail.com>
2022-09-24 21:40:10 -0300

    resposta de solicitação

--- testes.js
+++ testes.js
@@ -1,6 +1,19 @@
 const http = require('http')
 
 http.createServer((req, res)=>{
-    res.writeHead(200, {"content-type":"text/plain"})
-    res.end("meu primeiro servidor!")
+    res.writeHead(200, {"content-type":"text/plain"});
+
+    switch(req.url){
+        case "/":
+            res.end("você está na página home!")
+            break;
+
+           case "/contato":
+            res.end("você está na página contato!")
+            break;
+    }
+
+    res.end("meu primeiro");
+
+    
 }).listen(1914);
